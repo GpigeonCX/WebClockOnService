9e74770d2f11b8595e4de1ece2418c13c8b51e66
Perry <137569042@qq.com>
2019-08-02 22:29:19 +0800

    Index更新

--- WebMVC/Controllers/HomeController.cs
+++ WebMVC/Controllers/HomeController.cs
@@ -33,7 +33,7 @@ namespace WebMVC.Controllers
                             select new //待优化，格式化放到前台去处理，速度会快
                             {
                                 CardId = r.CardId,//工号
-                                EmployeeName = r.EmployeeName,//名称
+                                EmployeeName = r.EmployeeName.ToString(),//名称
                                 LastClockTime = r.LastClockTime.ToString(),//上次打卡时间
                                 ClockStateAM = r.ClockStateAM,//打卡状态
                                 ClockStatePM = r.ClockStatePM,
@@ -68,128 +68,6 @@ namespace WebMVC.Controllers
                 var result = new { total, rows = data1.Skip(pageSize * (pageIndex - 1)).Take(pageSize) };
 
                 return Json(result, JsonRequestBehavior.AllowGet);
-                #region 遗弃代码
-                //if (!string.IsNullOrEmpty(Request["EmployeeName"]) && !string.IsNullOrEmpty(Request["CardId"]))
-                //{
-                //    EmployeeName = Request["EmployeeName"].ToString();
-                //    CardId = Request["CardId"].ToString();
-
-                //    var data1 = (from r in Db.ClockModels.ToList()
-                //                 where r.EmployeeName.Contains(EmployeeName) && r.CardId.Equals(CardId)
-                //                 select new
-                //                 {
-                //                     CardId = r.CardId,//工号
-                //                     EmployeeName = r.EmployeeName,//名称
-                //                     LastClockTime = r.LastClockTime.ToString().CompareTo("2010-1-1 0:00:00") > 0 ? r.LastClockTime.ToString() : "",//上次打卡时间
-                //                     ClockStateAM = r.ClockStateAM,//打卡状态
-                //                     ClockStatePM = r.ClockStatePM,
-                //                     TotalDays = r.TotalDays,//打卡天数
-                //                     SurplusDay = r.CreatTime.AddDays(r.TotalDays).Subtract(DateTime.Now).Days,//剩余天数
-                //                     OnTimeStart = r.OnTimeStart.ToString(),//上班打卡开始时间
-                //                     OnTimeEnd = r.OnTimeEnd.ToString(),//上班打卡结束时间
-                //                     OffTimeStart = r.OffTimeStart.ToString(),//下班打卡开始时间
-                //                     OffTimeEnd = r.OffTimeEnd.ToString(),//下班打卡结束时间
-                //                     NeedSta = r.NeedSta,//周六打卡
-                //                     NeedSun = r.NeedSun,//周日打卡
-                //                     r.flag,
-                //                     r.PassWord,
-                //                 }
-                //                  )
-                //                  .Skip(pageSize * (pageIndex - 1)).Take(pageSize).ToList();
-                //    int total = Db.ClockModels.Count();//总条数
-                //                                       //构造成Json的格式传递
-                //    var result = new { total = total, rows = data1 };
-                //    return Json(result, JsonRequestBehavior.AllowGet);
-                //}
-                //else if (!string.IsNullOrEmpty(Request["UserName"]) && string.IsNullOrEmpty(Request["CardId"]))
-                //{
-                //    EmployeeName = Request["UserName"].ToString();
-
-                //    var data1 = (from r in Db.ClockModels.ToList()
-                //                 where r.EmployeeName.Contains(EmployeeName)
-                //                 select new
-                //                 {
-                //                     CardId = r.CardId,//工号
-                //                     EmployeeName = r.EmployeeName,//名称
-                //                     LastClockTime = r.LastClockTime.ToString().CompareTo("2010-1-1 0:00:00") > 0 ? r.LastClockTime.ToString() : "",//上次打卡时间
-                //                     ClockStateAM = r.ClockStateAM,//打卡状态
-                //                     ClockStatePM = r.ClockStatePM,
-                //                     TotalDays = r.TotalDays,//打卡天数
-                //                     SurplusDay = r.CreatTime.AddDays(r.TotalDays).Subtract(DateTime.Now).Days,//剩余天数
-                //                     OnTimeStart = r.OnTimeStart.ToString(),//上班打卡开始时间
-                //                     OnTimeEnd = r.OnTimeEnd.ToString(),//上班打卡结束时间
-                //                     OffTimeStart = r.OffTimeStart.ToString(),//下班打卡开始时间
-                //                     OffTimeEnd = r.OffTimeEnd.ToString(),//下班打卡结束时间
-                //                     NeedSta = r.NeedSta,//周六打卡
-                //                     NeedSun = r.NeedSun,//周日打卡
-                //                     r.flag,
-                //                     r.PassWord,
-                //                 }
-                //                  )
-                //                  .Skip(pageSize * (pageIndex - 1)).Take(pageSize).ToList();
-                //    int total = Db.ClockModels.Count();//总条数
-                //                                       //构造成Json的格式传递
-                //    var result = new { total = total, rows = data1 };
-                //    return Json(result, JsonRequestBehavior.AllowGet);
-                //}
-                //else if (string.IsNullOrEmpty(Request["UserName"]) && !string.IsNullOrEmpty(Request["CardId"]))
-                //{
-                //    CardId = Request["CardId"].ToString();
-                //    var data1 = (from r in Db.ClockModels.ToList()
-                //                 where r.CardId.Equals(CardId)
-                //                 select new
-                //                 {
-                //                     CardId = r.CardId,//工号
-                //                     EmployeeName = r.EmployeeName,//名称
-                //                     LastClockTime = r.LastClockTime.ToString().CompareTo("2010-1-1 0:00:00") > 0 ? r.LastClockTime.ToString() : "",//上次打卡时间
-                //                     ClockStateAM = r.ClockStateAM,//打卡状态
-                //                     ClockStatePM = r.ClockStatePM,
-                //                     SurplusDay = r.CreatTime.AddDays(r.TotalDays).Subtract(DateTime.Now).Days,//剩余天数
-                //                     OnTimeStart = r.OnTimeStart.ToString(),//上班打卡开始时间
-                //                     OnTimeEnd = r.OnTimeEnd.ToString(),//上班打卡结束时间
-                //                     OffTimeStart = r.OffTimeStart.ToString(),//下班打卡开始时间
-                //                     OffTimeEnd = r.OffTimeEnd.ToString(),//下班打卡结束时间
-                //                     NeedSta = r.NeedSta,//周六打卡
-                //                     NeedSun = r.NeedSun,//周日打卡
-                //                     r.flag,
-                //                     r.PassWord,
-                //                 }
-                //                  )
-                //                  .Skip(pageSize * (pageIndex - 1)).Take(pageSize).ToList();
-                //    int total = Db.ClockModels.Count();//总条数
-                //                                       //构造成Json的格式传递
-                //    var result = new { total = total, rows = data1 };
-                //    return Json(result, JsonRequestBehavior.AllowGet);
-                //}
-                //else
-                //{
-                //    var data1 = (from r in Db.ClockModels.ToList()
-                //                 select new
-                //                 {
-                //                     CardId = r.CardId,//工号
-                //                     EmployeeName = r.EmployeeName,//名称
-                //                     LastClockTime = r.LastClockTime.ToString().CompareTo("2010-1-1 0:00:00") > 0 ? r.LastClockTime.ToString() : "",//上次打卡时间
-                //                     ClockStateAM = r.ClockStateAM,//打卡状态
-                //                     ClockStatePM = r.ClockStatePM,
-                //                     TotalDays = r.TotalDays,//打卡天数
-                //                     SurplusDay = r.CreatTime.AddDays(r.TotalDays).Subtract(DateTime.Now).Days,//剩余天数
-                //                     OnTimeStart = r.OnTimeStart.ToString(),//上班打卡开始时间
-                //                     OnTimeEnd = r.OnTimeEnd.ToString(),//上班打卡结束时间
-                //                     OffTimeStart = r.OffTimeStart.ToString(),//下班打卡开始时间
-                //                     OffTimeEnd = r.OffTimeEnd.ToString(),//下班打卡结束时间
-                //                     NeedSta = r.NeedSta,//周六打卡
-                //                     NeedSun = r.NeedSun,//周日打卡
-                //                     r.flag,
-                //                     r.PassWord,
-                //                 }
-                //  )
-                //  .Skip(pageSize * (pageIndex - 1)).Take(pageSize).ToList();
-                //    int total = Db.ClockModels.Count();//总条数
-                //                                       //构造成Json的格式传递
-                //    var result = new { total = total, rows = data1 };
-                //    return Json(result, JsonRequestBehavior.AllowGet);
-                //}
-                #endregion
             }
             catch (Exception ex)
             {
@@ -345,7 +223,7 @@ namespace WebMVC.Controllers
                     {
                         toupdateitem.CardId = enlist[i].CardId.Trim();
                         toupdateitem.PassWord = enlist[i].PassWord.Trim();
-                        toupdateitem.EmployeeName = enlist[i].EmployeeName;
+                        toupdateitem.EmployeeName = enlist[i].EmployeeName ?? "NULL";
                         toupdateitem.TotalDays = enlist[i].TotalDays;
                         toupdateitem.OnTimeStart = enlist[i].OnTimeStart;
                         toupdateitem.OnTimeEnd = enlist[i].OnTimeEnd;
@@ -530,84 +408,5 @@ namespace WebMVC.Controllers
             }
         }
 
-        #region 遗弃的txt导入导出
-        [HttpPost]
-        public ActionResult UploadFile()
-        {
-            HttpPostedFile file = System.Web.HttpContext.Current.Request.Files[0];
-            if (file == null)
-                return Json("文件上传失败!");//验证文件
-
-            string FileType = file.FileName.Split('.').Last().ToUpper();
-            if (FileType != "TXT")
-                return Json("文件上传失败,请上传模板格式为txt的文件");//验证文件
-            try
-            {
-                Stream fileStream = file.InputStream;
-                StreamReader reader = new StreamReader(fileStream, Encoding.Default);
-                string[] StrDat = reader.ReadToEnd().Split(new char[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
-                var data1 = from r in Db.ClockModels select new { r.CardId };
-
-                foreach (var item in StrDat.Skip(1))
-                {
-                    var line = item.Split('-');
-                    ClockModels model = new ClockModels()
-                    {
-                        CardId = line[0],
-                        EmployeeName = line[1],
-                        TotalDays = Convert.ToInt32(line[2]),
-                        OnTimeStart = Convert.ToDateTime(line[3]),
-                        OnTimeEnd = Convert.ToDateTime(line[4]),
-                        OffTimeStart = Convert.ToDateTime(line[5]),
-                        OffTimeEnd = Convert.ToDateTime(line[6]),
-                        NeedSta = line[7] == "0" ? false : true,
-                        NeedSun = line[8] == "0" ? false : true,
-                        //默认数据
-                        CreatTime = DateTime.Now,
-                        LastClockTime = Convert.ToDateTime("2000-1-1 00:00"),
-                        ClockStateAM = false,
-                        ClockStatePM = false,
-                        flag = true,
-                    };
-                    Db.ClockModels.Add(model);
-                    Db.Entry<ClockModels>(model).State = EntityState.Added;
-                }
-                int countNum = Db.SaveChanges();
-                return countNum > 0 ? Json($"成功导入{countNum}条数据!") : Json("添加失败,请检查数据格式及工号是否重复！");
-            }
-            catch (Exception ex)
-            {
-                return Json($"添加失败,请检查数据格式及工号是否重复！异常信息：{ex}");
-            }
-        }
-        public ActionResult ExportFile()
-        {
-            string id = Request["cardids"];
-            string[] ids = id.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
-            IEnumerable<ClockModels> data = Db.ClockModels.Where(x => ids.Contains(x.CardId)).ToList();
-            StringBuilder result = new StringBuilder();
-            result.Append("工号-名称-最后打卡时间-上午打卡状态-下午打卡状态-总天数-剩余天数-OnStart-OnEnd-OffStart-OffEnd-周六-周日\r\n");
-            foreach (var model in data)
-            {
-                result.Append(model.CardId + "-"
-                            + model.EmployeeName + "-"
-                            + model.LastClockTime + "-"
-                            + model.ClockStateAM + "-"
-                            + model.ClockStatePM + "-"
-                            + model.TotalDays + "-"
-                            + model.CreatTime.AddDays(model.TotalDays).Subtract(DateTime.Now).Days + "-"
-                            + model.OnTimeStart.ToString() + "-"
-                            + model.OnTimeEnd.ToString() + "-"
-                            + model.OffTimeStart.ToString() + "-"
-                            + model.OffTimeEnd.ToString() + "-"
-                            + model.NeedSta + "-"
-                            + model.NeedSun + "\r\n"
-                            );
-
-            }
-            byte[] bytearry = Encoding.UTF8.GetBytes(result.ToString());
-            return File(bytearry, "text/plain", string.Format("导出数据{0}.txt", DateTime.Now));
-        }
-        #endregion
     }
 }
